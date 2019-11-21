b9b5da07d163e90b7d33ee960ee2fe6c027e8caa
Jonathan Jones <me@xayah.net>
2019-11-21 12:48:01 -0500

    add proportion confidence intervals and kappas to agreement testing

--- agr_testing.R
+++ agr_testing.R
@@ -18,3 +18,10 @@ exp_agr <- (nrow(filter(agr_data, Jonathan=="ambiguous"))/200) * (nrow(filter(ag
   (nrow(filter(agr_data, Michael %in% c("continuative", "iterative")))/200) * (nrow(filter(agr_data, Jonathan=="ambiguous"))/200) * 0.5
 act_agr <- sum(agr_data$agree)/200
 kappa <- (act_agr - exp_agr) / (1 - exp_agr)
+
+error = sqrt(act_agr * (1 - act_agr) / 200) * 1.96
+ci.low = act_agr - error
+ci.high = act_agr + error
+
+kappa.low <- (ci.low - exp_agr) / (1 - exp_agr)
+kappa.high <- (ci.high - exp_agr) / (1 - exp_agr)
